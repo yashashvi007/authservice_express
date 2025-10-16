22146103545dbfc597eeeeb4422d6d410680e9b7
yashashvi <yashashvimaurya@gmail.com>
2025-10-17 00:37:25 +0530

    Update project configuration and add initial application structure
    - Updated .gitignore to include .env file.
    - Modified package.json to add dotenv, express, and winston dependencies, along with dev scripts for nodemon and ts-node.
    - Created app.ts for the Express application setup.
    - Enhanced server.ts to start the server and log the running port.
    - Added configuration management with dotenv in config/index.ts.
    - Implemented logging functionality using winston in config/logger.ts.

---
+++ src/config/logger.ts
@@ -0,0 +1,30 @@
+import winston from 'winston';
+import { Config } from '.';
+
+const logger = winston.createLogger({
+  level: 'info',
+  defaultMeta: { service: 'auth-service' },
+  transports: [
+    new winston.transports.File({
+      level: 'info',
+      dirname: 'logs',
+      filename: 'app.logs',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+      silent: Config.NODE_ENV === 'test',
+    }),
+    new winston.transports.File({
+      level: 'error',
+      dirname: 'logs',
+      filename: 'error.logs',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+      silent: Config.NODE_ENV === 'test',
+    }),
+    new winston.transports.Console({
+      level: 'info',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+      silent: Config.NODE_ENV === 'test',
+    }),
+  ],
+});
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-function main(x: number, y: number): number {
-  return x + y;
-}
+import app from './app';
+import { Config } from './config';
+import logger from './config/logger';
+
+const startServer = () => {
+  const PORT = Config.PORT;
+
+  try {
+    app.listen(PORT, () => logger.info(`Server is running on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+startServer();
