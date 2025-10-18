adce4b890b7009a57996943d09d00d59d2ac4c3a
yashashvi <yashashvimaurya@gmail.com>
2025-10-18 23:59:42 +0530

    Add http-errors dependency and implement error handling middleware in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
+import logger from './config/logger';
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.use((err: HttpError, req: Request, res: Response, _next: NextFunction) => {
+  logger.error(err.message);
+
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: '',
+        location: '',
+      },
+    ],
+  });
 });
 
 export default app;
