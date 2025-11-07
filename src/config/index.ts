ca6381fcad07ed23f5e2d0b14a17134196f95dec
yashashvi <yashashvimaurya@gmail.com>
2025-11-07 19:24:02 +0530

    added migrations

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import path from 'path';
 
-config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || 'dev'}`) });
 
 const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME, REFRESH_TOKEN_SECRET, JWKS_URI } =
   process.env;
--- src/data-source.ts
+++ src/data-source.ts
@@ -15,6 +15,6 @@ export const AppDataSource = new DataSource({
   synchronize: Config.NODE_ENV === 'dev',
   logging: false,
   entities: [User, RefreshToken],
-  migrations: [],
+  migrations: ['src/migration/*.ts'],
   subscribers: [],
 });
